af133b3a967f251cb61732dfe644bea1d821aee9
mtakagi <mtakagi127@live.jp>
2017-12-12 04:15:40 +0900

    Add UInt8 array decode tests

--- Tests/MsgPackEncoderTests/UInt8ArrayTests.swift
+++ Tests/MsgPackEncoderTests/UInt8ArrayTests.swift
@@ -18,6 +18,14 @@ class UInt8ArrayTests : XCTestCase {
         XCTAssertEqual(encoded, result)
     }
 
+    func testDecodeUInt8Array8() {
+        let data = [UInt8](repeating: 0, count: 0)
+        let decoder = MessagePackDecoder()
+        let result = try! decoder.decode(Data.self, from: Data(bytes: [0xc4, 0x00]) + data)
+
+        XCTAssertEqual(result!, Data(data))
+    }
+
     func testEncodeUInt8Array8ToArray() {
         let data = [UInt8](repeating: 0, count: 0)
         let result = Data(bytes: [0b10010000]) + data
@@ -25,6 +33,14 @@ class UInt8ArrayTests : XCTestCase {
         encoder.uint8ArrayEncodingStrategy = .array
         let encoded = try! encoder.encode(data)
         XCTAssertEqual(encoded, result)
+    }
+
+    func testDecodeUInt8Array8ToArray() {
+        let data = [UInt8](repeating: 0, count: 0)
+        let decoder = MessagePackDecoder()
+        let result = try! decoder.decode([UInt8].self, from: Data(bytes: [0b10010000]) + data)
+
+        XCTAssertEqual(result!, data)
     }
 
     func testEncodeUInt8Array8Max() {
@@ -35,12 +51,28 @@ class UInt8ArrayTests : XCTestCase {
         XCTAssertEqual(encoded, result)
     }
 
+    func testDecodeUInt8Array8Max() {
+        let data = [UInt8](repeating: 0, count: 0xff)
+        let decoder = MessagePackDecoder()
+        let result = try! decoder.decode(Data.self, from: Data(bytes: [0xc4, 0xff]) + data)
+
+        XCTAssertEqual(result!, Data(data))
+    }
+
     func testEncodeUInt8Array16() {
         let data = [UInt8](repeating: 0, count: 0x100)
         let result = Data(bytes: [0xc5, 0x01, 0x00]) + data
         let encoder = MessagePackEncoder()
         let encoded = try! encoder.encode(data)
         XCTAssertEqual(encoded, result)
+    }
+
+    func testDecodeUInt8Array16() {
+        let data = [UInt8](repeating: 0, count: 0x100)
+        let decoder = MessagePackDecoder()
+        let result = try! decoder.decode(Data.self, from: Data(bytes: [0xc5, 0x01, 0x00]) + data)
+
+        XCTAssertEqual(result!, Data(data))
     }
 
     func testEncodeUInt8Array16ToArray() {
@@ -52,12 +84,28 @@ class UInt8ArrayTests : XCTestCase {
         XCTAssertEqual(encoded, result)
     }
 
+    func testDecodeUInt8Array16ToArray() {
+        let data = [UInt8](repeating: 0, count: 0x100)
+        let decoder = MessagePackDecoder()
+        let result = try! decoder.decode([UInt8].self, from: Data(bytes: [0xdc, 0x01, 0x00]) + data)
+
+        XCTAssertEqual(result!, data)
+    }
+
     func testEncodeUInt8Array32() {
         let data = [UInt8](repeating: 0, count: 0x10000)
         let result = Data(bytes: [0xc6, 0x00, 0x01, 0x00, 0x00]) + data
         let encoder = MessagePackEncoder()
         let encoded = try! encoder.encode(data)
         XCTAssertEqual(encoded, result)
+    }
+
+    func testDecodeUInt8Array32() {
+        let data = [UInt8](repeating: 0, count: 0x10000)
+        let decoder = MessagePackDecoder()
+        let result = try! decoder.decode(Data.self, from: Data(bytes: [0xc6, 0x00, 0x01, 0x00, 0x00]) + data)
+
+        XCTAssertEqual(result!, Data(data))
     }
 
     func testEncodeUInt8Array32ToArray() {
@@ -69,13 +117,27 @@ class UInt8ArrayTests : XCTestCase {
         XCTAssertEqual(encoded, result)
     }
 
+    func testDecodeUInt8Array32ToArray() {
+        let data = [UInt8](repeating: 0, count: 0x10000)
+        let decoder = MessagePackDecoder()
+        let result = try! decoder.decode([UInt8].self, from: Data(bytes: [0xdd, 0x00, 0x01, 0x00, 0x00]) + data)
+
+        XCTAssertEqual(result!, data)
+    }
+
     static var allTests = [
         ("testEncodeUInt8Array8", testEncodeUInt8Array8),
+        ("testDecodeUInt8Array8", testDecodeUInt8Array8),
         ("testEncodeUInt8Array8ToArray", testEncodeUInt8Array8ToArray),
+        ("testDecodeUInt8Array8ToArray", testDecodeUInt8Array8ToArray),
         ("testEncodeUInt8Array8Max", testEncodeUInt8Array8Max),
+        ("testDecodeUInt8Array8Max", testDecodeUInt8Array8Max),
         ("testEncodeUInt8Array16", testEncodeUInt8Array16),
+        ("testDecodeUInt8Array16", testDecodeUInt8Array16),
         ("testEncodeUInt8Array16ToArray", testEncodeUInt8Array16ToArray),
         ("testEncodeUInt8Array32", testEncodeUInt8Array32),
+        ("testDecodeUInt8Array32", testDecodeUInt8Array32),
         ("testEncodeUInt8Array32ToArray", testEncodeUInt8Array32ToArray),
+        ("testDecodeUInt8Array32ToArray", testDecodeUInt8Array32ToArray),
     ]
 }
